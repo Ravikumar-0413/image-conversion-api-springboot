86ea11adaec6a9101c801657b1263906e9d73363
Ravi Kumar <pvlravikumar@gmail.com>
2026-04-07 19:08:25 +0530

    Fix CORS issue for Vercel

--- src/main/java/com/IC/image/converter/congif/CorsConfig.java
+++ src/main/java/com/IC/image/converter/congif/CorsConfig.java
@@ -3,6 +3,18 @@ package com.IC.image.converter.congif;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.*;
 
+// @Configuration
+// public class CorsConfig implements WebMvcConfigurer {
+
+//     @Override
+//     public void addCorsMappings(CorsRegistry registry) {
+
+//         registry.addMapping("/**")
+//                 .allowedOrigins("http://localhost:3000") // your frontend
+//                 .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                 .allowedHeaders("*");
+//     }
+// }
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
 
@@ -10,8 +22,12 @@ public class CorsConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
 
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000") // your frontend
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "https://image-tool-murex.vercel.app"
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
